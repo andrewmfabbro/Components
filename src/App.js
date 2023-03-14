153f0b0521e076169d5af134465fb1381b5c443c
Andrew <andrewmfabbro@gmail.com>
2023-03-13 20:52:54 -0400

    dropdown boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import Accordion from "./components/Accordion";
+import { getValue } from "@testing-library/user-event/dist/utils";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-        id:'jl343',
-      label: "Can I use React on a project?",
-      content: "You can use React on any project you like!",
-    },
-    {
-        id: "32lewkrj",
-      label: "Can I use JavaScript?",
-      content: "You can use JavaScript on any project you like!",
-    },
-    {
-        id:"3j42lte",
-      label: "Can I use CSS?",
-      content: "You can use CSS on any project you like!",
-    },
-  ];
-
-  return <Accordion items={items}/>;
+  const options = [
+    {label: 'Red', value: 'red'},
+    {label: 'Green', value: 'green'},
+    {label: 'Blue', value: 'blue'}
+  ]
+return <Dropdown options={options}/>
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,25 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+        id:'jl343',
+      label: "Can I use React on a project?",
+      content: "You can use React on any project you like!",
+    },
+    {
+        id: "32lewkrj",
+      label: "Can I use JavaScript?",
+      content: "You can use JavaScript on any project you like!",
+    },
+    {
+        id:"3j42lte",
+      label: "Can I use CSS?",
+      content: "You can use CSS on any project you like!",
+    },
+  ];
+
+  return <Accordion items={items}/>;
+}
+
+export default AccordionPage;
