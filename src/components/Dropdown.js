411c68079dd5b270390b7b572c82e84b07e86f1d
Andrew <andrewmfabbro@gmail.com>
2023-03-16 17:14:20 -0400

    added event handlers and state for dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,33 @@
-function Dropdown(){
-    return <div>Dropdown...</div>
+import { useState } from "react";
+
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((currentIsOpen) => !currentIsOpen); //functional stops stale state
+  };
+
+  const handleOptionClick = (option) => {
+    //CLOSE DROPDOWN
+    setIsOpen(false);
+    //WHAT OPTION DID THE USER CLICK?
+    console.log(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={()=>handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
