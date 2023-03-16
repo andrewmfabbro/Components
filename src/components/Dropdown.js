c8a80b153d768e8c43966bedda07dcbb97c47b05
Andrew <andrewmfabbro@gmail.com>
2023-03-16 19:01:14 -0400

    made panel component and styled dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -11,26 +13,35 @@ function Dropdown({ options, selection, onSelect }) {
     //CLOSE DROPDOWN
     setIsOpen(false);
     //WHAT OPTION DID THE USER CLICK?
-    onSelect(option);
+    onChange(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
-  let content = "Select...";
-  if (selection) {
-    content = selection.label;
-  }
-
   return (
-    <div>
-      <div onClick={handleClick}>{content}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <Panel
+        className="flex-justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg"/>
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full">
+          {renderedOptions}
+        </Panel>
+      )}
     </div>
   );
 }
