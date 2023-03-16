d3c11a4d16d10dbbc9067e50a99590909bdfab5a
Andrew <andrewmfabbro@gmail.com>
2023-03-16 17:23:43 -0400

    finished event handler function and connected prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { getValue } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
   const options = [
-    {label: 'Red', value: 'red'},
-    {label: 'Green', value: 'green'},
-    {label: 'Blue', value: 'blue'}
-  ]
-return <Dropdown options={options}/>
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+  return <Dropdown options={options} selection={selection} onSelect={handleSelect} />;
 }
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, onSelect }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -11,20 +11,25 @@ function Dropdown({ options }) {
     //CLOSE DROPDOWN
     setIsOpen(false);
     //WHAT OPTION DID THE USER CLICK?
-    console.log(option);
+    onSelect(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={()=>handleOptionClick(option)} key={option.value}>
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
         {option.label}
       </div>
     );
   });
 
+  let content = "Select...";
+  if (selection) {
+    content = selection.label;
+  }
+
   return (
     <div>
-      <div onClick={handleClick}>Select...</div>
+      <div onClick={handleClick}>{content}</div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
