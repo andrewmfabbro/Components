3d8e33545b4401f824c4bd37e89fbd3e6bec3656
Andrew <andrewmfabbro@gmail.com>
2023-04-17 16:09:52 -0400

    created counter and made it into a reusable hook

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
-      <Sidebar/>
+      <Sidebar />
       <div className="col-span-5">
         <Route path="/accordion">
           <AccordionPage />
@@ -18,13 +19,16 @@ function App() {
           <DropdownPage />
         </Route>
         <Route path="/buttons">
-          <ButtonPage/>
+          <ButtonPage />
         </Route>
         <Route path="/modal">
-          <ModalPage/>
+          <ModalPage />
         </Route>
         <Route path="/table">
-          <TablePage/>
+          <TablePage />
+        </Route>
+        <Route path="/counter">
+          <CounterPage initialCount={1} />
         </Route>
       </div>
     </div>
