0d9916bf6fbe44514aaecd031a2a850c0a8a34aa
Andrew <andrewmfabbro@gmail.com>
2023-04-17 20:39:08 -0400

    refactor with immer lib produce for in CounterPage

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -10,37 +11,29 @@ const ADD_VALUE_TO_COUNT = "add_value_to_count";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count +1;
+      return;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload;
+      return;
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count-1;
+      return;
     case ADD_VALUE_TO_COUNT:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd =0;
+      return;
     default:
       //two differnet ways to go pick one throw error or return state
       //throw new Error('unexpected action type: '+ action.type);
-      return state;
+      return;
   }
 };
 
 function CounterPage({ initialCount }) {
   //const [count, setCount] = useState(initialCount);
   //const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
